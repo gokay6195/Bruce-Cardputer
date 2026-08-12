0369de6099ddf733580ed90867fefc193fd368ef
gokay6195 <gokay6195@gmail.com>
2026-08-13 00:59:03 +0200

    Refactor RFID2.h by updating version and cleaning code
    Updated version number and removed commented code.

--- src/modules/rfid/RFID2.h
+++ src/modules/rfid/RFID2.h
@@ -2,7 +2,7 @@
  * @file RFID2.h
  * @author Rennan Cockles (https://github.com/rennancockles)
  * @brief Read and Write RFID tags using RFID2 module from M5Stack
- * @version 0.2 - Improved by community
+ * @version 0.1
  * @date 2024-08-19
  */
 
@@ -11,27 +11,12 @@
 #include <MFRC522DriverPinSimple.h>
 #include <MFRC522v2.h>
 
-// Nouveau code retour : lecture partielle (certains secteurs verrouillés)
-#define PARTIAL_SUCCESS 5
-
 class RFID2 : public RFIDInterface {
 public:
     MFRC522 mfrc522;
-
-    /////////////////////////////////////////////////////////////////////////////////////
-    // Constructor
-    /////////////////////////////////////////////////////////////////////////////////////
     RFID2(bool use_i2c = true);
     ~RFID2();
-
-    /////////////////////////////////////////////////////////////////////////////////////
-    // Life Cycle
-    /////////////////////////////////////////////////////////////////////////////////////
     bool begin();
-
-    /////////////////////////////////////////////////////////////////////////////////////
-    // Operations
-    /////////////////////////////////////////////////////////////////////////////////////
     int read(int cardBaudRate = 0);
     int clone();
     int erase();
@@ -44,32 +29,19 @@ private:
     bool _use_i2c;
     MFRC522Driver *_driver;
     MFRC522DriverPinSimple ss_pin = MFRC522DriverPinSimple(SPI_SS_PIN);
-
-    /////////////////////////////////////////////////////////////////////////////////////
-    // Converters
-    /////////////////////////////////////////////////////////////////////////////////////
     void format_data();
     void parse_data();
     void set_uid();
-
-    /////////////////////////////////////////////////////////////////////////////////////
-    // PICC Helpers
-    /////////////////////////////////////////////////////////////////////////////////////
     bool PICC_IsNewCardPresent();
-
     String get_tag_type();
     int read_data_blocks();
     int read_mifare_classic_data_blocks(byte piccType);
     int read_mifare_classic_data_sector(byte sector);
     int authenticate_mifare_classic(byte block);
     int read_mifare_ultralight_data_blocks();
-
     int write_data_blocks();
-    // Signature mise à jour : do_auth=false permet d'éviter de ré-authentifier
-    // à chaque bloc quand on écrit plusieurs blocs d'un même secteur
-    bool write_mifare_classic_data_block(int block, String data, bool do_auth = true);
+    bool write_mifare_classic_data_block(int block, String data);
     bool write_mifare_ultralight_data_block(int block, String data);
-
     int erase_data_blocks();
     int write_ndef_blocks();
 };
